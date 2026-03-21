262bb7fcfacc26dde76d09709ec0386cb5f6fae4
Luiz-M-Marangon <luizmarangonzz11@gmail.com>
2026-03-21 16:06:36 -0300

    criação do DAO

--- src/main/java/org/example/dao/MotoristaDAO.java
+++ src/main/java/org/example/dao/MotoristaDAO.java
@@ -1,4 +1,20 @@
 package org.example.dao;
 
+import jakarta.persistence.EntityManager;
+import org.example.model.Motorista;
+import org.example.util.JPAUtil;
+import org.hibernate.Session;
+
 public class MotoristaDAO {
+
+    public void salvar(Motorista motorista){
+        EntityManager em = JPAUtil.getEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(motorista);
+
+        em.getTransaction().commit();
+        em.close();
+    }
 }
